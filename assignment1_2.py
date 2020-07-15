584ce8e4a276b3d76b88fc03b0f78e1e856ace65
natthamon55 <64399206+natthamon55@users.noreply.github.com>
2020-07-15 18:14:00 +0700

    Add files via upload

--- assignment1_2.py
+++ assignment1_2.py
@@ -6,7 +6,9 @@
 ###assignment1
 import pygame 
 import random
-import math
+
+black = 0,0,0
+white = 255,255,255
 # initialize PyGame
 pygame.init()
 
@@ -18,12 +20,11 @@ clock = pygame.time.Clock()
 #set up screen
 scr_w, scr_h = 800, 600
 screen  = pygame.display.set_mode((scr_w, scr_h))
-surface = pygame.Surface( screen.get_size(), pygame.SRCALPHA )
+
 #list of info
 cir_cle = []
-Name_cir = []
-radius_cir = []
-
+Name = []
+radius = []
 #random color rbg
 COLORS = [(119, 141, 252), 
           (119, 252, 181),
@@ -33,28 +34,57 @@ COLORS = [(119, 141, 252),
 
 def random_color():
     return random.choice(COLORS)
+
 #make n=10 circles
 def draw_circle():
     for n in range(10):
         r = random.randint(10,20) #random radius 10-20 
-        x,y = random.randint(r,scr_w-r), random.randint(r,scr_h-r) #random x,y position
+        x = random.randint(r,scr_w-r) #random x position
+        y = random.randint(r,scr_h-r) #random y position
         color= random_color() #random color
-        draw_cir=pygame.draw.circle( surface, color, (x,y), r ) #make circle
+        draw_cir=pygame.draw.circle( screen, color, (x,y), r ,0) #make circle
 
-        Name_cir.append(draw_cir)
+        #add info
+        Name.append(draw_cir)
         cir_cle.append((x,y,r))
-        radius_cir.append(r)
+        radius.append(r)
 
-    return True
+    return 0
+
+# delete the most radius of circles
+def get_cir_rm():
+    max_r = max(radius)
+    for x,y,r in cir_cle :
+        if r == max_r: 
+            c = cir_cle[cir_place.index((x,y,r))]   # Let c = circle with the most radius
+            index = cir_place.index((x,y,r))          # find index of circle with the most radius
+            circle = Name[index]                      # Let circle = val of  circle 
+            print("The biggest circle is ", circle)
+            return circle
+
+# get the most radius
+def get_r_rm():
+    max_r = max(radius)
+    for x,y,r in cir_cle :
+        if r == max_r: 
+            return r 
 
 
+def rm_data():
+    circle = get_cir_rm()                     # circle = วงกลมที่จะลบ
+    index = Name.index(circle)                # fin dindex of data about circle
+    Name.remove(Name[index])                  # Remove val from Name
+    cir_cle.remove(cir_cle[index])        # Remove position from cir_place
+    radius.remove(radius[index])              # Remove radius from radius
+    return 0    
+
+#do circle
 draw_circle()
-
-
 
 # Run until the user asks to quit
 running = True
 while running:
+
     clock.tick( 10 ) 
 
     # Did the user click the window close button?
@@ -70,15 +100,8 @@ while running:
                 r = get_r_rm()
                 if ( circle_remove.collidepoint( mouse_pos ) ):   
                     pygame.draw.circle(screen, black, mouse_pos, r*2 ,0)    
-                    del_data()                                  
+                    rm_data()                                  
     
-    
-    # fill the screen with the white color
-    screen.fill((255,255,255))
-
-    # draw the surface on the screen
-    screen.blit(surface, (0,0))
-
     # update the screen display
     pygame.display.update()
 
