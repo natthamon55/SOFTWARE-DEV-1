0e9c758669b1f38b9fc429ab2b6982adb9cb42bb
natthamon55 <64399206+natthamon55@users.noreply.github.com>
2020-09-26 09:01:20 +0700

    Update score gui.py

--- Game_Project/GUI/score gui.py
+++ Game_Project/GUI/score gui.py
@@ -1,7 +1,4 @@
-#Game Progect : ONE TOUCH (Pygame & Python Arcade)
-#Blog Update : https://s6201012620139.blogspot.com/2020/09/one-touch-game-project-010123131.html
-#student id : 1)นางสาวหทัยพัทร ชำนินวล 6201012620244 2) นางสาวนัทธมน บุญนิธิ  6201012620139
-#----------------------------------------------------------------------------------------------#
+
 import pygame
 import sys
 
